a8705bd48b5aa9085b259941af4e2cc978713182
Bon <bng22061996@gmail.com>
2017-12-02 14:24:14 +0700

    update Post

--- webraovat/src/Database/DB.java
+++ webraovat/src/Database/DB.java
@@ -18,11 +18,11 @@ public class DB {
 	public void Connect() {
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-<<<<<<< HEAD
+
 			String url = "jdbc:sqlserver://DESKTOP-G21B0UL\\SQLEXPRESS:50290;databaseName=webraovat;user=sa; password=123";
-=======
-			String url = "jdbc:sqlserver://GOHANMYSTIC\\GOHANMYSTIC:1433;databaseName=webraovat;user=sa;password=123";
->>>>>>> 86adb1cbe67b69ad540d0f9eb717724d987479e2
+
+			//String url = "jdbc:sqlserver://GOHANMYSTIC\\GOHANMYSTIC:1433;databaseName=webraovat;user=sa;password=123";
+
 			cn = DriverManager.getConnection(url);
 			
 		}catch(Exception e) {
